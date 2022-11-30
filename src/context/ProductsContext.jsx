e6b20f36d59bffe73aba5d7021272b05df477d62
Marco Valdez <antem.marco@gmail.com>
2022-11-29 20:16:14 -0600

    Inicializado el Context para los Productos

--- src/context/ProductsContext.jsx
+++ src/context/ProductsContext.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { createContext, useState, useEffect, useContext } from 'react'
+import axios from 'axios'
 
-const ProductsContext = () => {
-  return (
-    <div>ProductsContext</div>
-  )
+const ProductsContext = createContext()
+
+const getProducts = () => {
+  let products = {}
+  axios
+    .get('https://ecomerce-master.herokuapp.com/api/v1/item')
+    .then((response) => {
+      // handle success
+      products = response.data.results
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
+  return products
 }
 
-export default ProductsContext
+const ProductsProvider = (props) => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    setProducts(getProducts)
+    console.log(products)
+  })
+
+  return (
+    <div />
+  )
+}
