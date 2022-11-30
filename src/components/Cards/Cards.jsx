fee7341c75edc16010c5e911cfb4b2e937f133ef
Arturo0214 <osvaldosuarezcruz@gmail.com>
2022-11-30 15:49:03 -0600

    feat: added the product list to the Cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,29 @@
+/* eslint-disable padded-blocks */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-multiple-empty-lines */
 import React from 'react'
+import './cards.css'
 
-const Cards = () => {
+const Cards = ({ products }) => {
+
   return (
-    <div>Cards</div>
+    <>
+      <div className='column is-4-desktop is-6-tablet'>
+        <div className='card-image'>
+          <figure className='image is-16by9'>
+            <img src={products.image} alt='Placeholder image' />
+          </figure>
+        </div>
+        <div className='card-content'>
+          <span className='tag is-danger'>{products.product_name} </span>
+          <div className='is-flex is-justify-content-space-between is-align-items-center'>
+            <div className='content mb-0'>
+              <p className='mb-0 is-size-7'>{products.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </>
   )
 }
 
