c6fb8b5e8286c2e48867e7846396f3fea55aa8e1
Marco Valdez <antem.marco@gmail.com>
2022-12-02 22:55:24 -0600

    fix: arregladas las cards para enviar a la pagina de detalles

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,27 +6,30 @@ import './cards.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button, Card } from 'react-bootstrap'
 import { useProductsContext } from '../../context/ProductsContext'
+import { Link } from 'react-router-dom'
 
 
 const Cards = ({ products }) => {
   const context = useProductsContext()
   return (
     <>
-      <Card
-        style={{ width: '15rem' }} onClick={() => {
-          context.setSelectedItem(products._id)
-          console.log(context.item) 
-        }}
-      >
-        <Card.Img variant='top' src={products.image} />
-        <Card.Body>
-          <Card.Title>{products.product_name}</Card.Title>
-          <Card.Text>
-            {products.description}
-          </Card.Text>
-          <Button variant='primary'>Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <Link to={`/details/${products._id}`}>
+        <Card
+          style={{ width: '15rem' }} onClick={() => {
+            context.setSelectedItem(products._id)
+            console.log(context.item) 
+          }}
+        >
+          <Card.Img variant='top' src={products.image} />
+          <Card.Body>
+            <Card.Title>{products.product_name}</Card.Title>
+            <Card.Text>
+              {products.description}
+            </Card.Text>
+            <Button variant='primary'>Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Link>
     </>
   )
 }
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+// import { Outlet } from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useParams /* useNavigate */ } from 'react-router-dom'
+import DetailsItem from './DetailsItem'
 
 const Details = () => {
+  const [itemDetails, setItemDetails] = useState({})
+  const [loadingDetails, setLoadingDetails] = useState(true)
+  const { pid } = useParams()
+
+  const fetchItem = async (id) => {
+    const IURL = `https://e-commerce-backend-production-ad56.up.railway.app/api/v1/item/${id}`
+    try {
+      const results = await axios.get(`${IURL}`)
+      setItemDetails(results.data)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchItem(pid)
+    setLoadingDetails(false)
+  }, [])
   return (
-    <div>Details</div>
+    <>
+      {
+      loadingDetails
+        ? <h1> Cargando </h1>
+        : <div>
+          <DetailsItem />
+          <h2>{itemDetails.product_name}</h2>
+          <h5>{itemDetails.category}</h5>
+          <h4>{itemDetails.brand}</h4>
+          <h4>{itemDetails.price}</h4>
+          <p>{itemDetails.sku}</p>
+          <p>{itemDetails.description}</p>
+          <button style={{ }}>Comprar</button>
+          {/* eslint-disable-next-line react/jsx-closing-tag-location */}
+        </div>
+    }
+
+    </>
   )
 }
 
