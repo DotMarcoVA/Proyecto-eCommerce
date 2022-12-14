c572fc3fb1404338bb4c7fc8136b817c7cef4f4b
Marco Valdez <antem.marco@gmail.com>
2022-12-13 20:03:00 -0600

    refactor: Corregidos problemas de Merge de Ramas

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,24 @@
-import { useContext } from 'react'
-import useForm from '../../hooks/useForm'
-import { loginUserService } from '../../services/userServices'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../context/AuthContext'
+import { loginUserService } from '../../services/userServices'
+import { Button, Form } from 'react-bootstrap'
+import useForm from '../../hooks/useForm'
 
 const Login = () => {
-  const { loginUser } = useContext(AuthContext)
   const navigate = useNavigate()
 
   const sendData = async (data) => {
     try {
-      const result = await loginUserService(data)
-      if (result.status === 200) {
-        const token = result.data.token
-        loginUser(token)
+      const response = await loginUserService(data)
+      if (response.status === 200) {
+        const token = response.data.token
+        window.localStorage.setItem('token', token)
         navigate('/')
+
+        console.log(response)
       }
     } catch (error) {
-      console.log(error)
+      console.log('Error: ', error)
     }
   }
 
@@ -27,7 +28,35 @@ const Login = () => {
   })
 
   return (
-    <div>Login</div>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className='mb-3' controlId='email'>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type='email'
+          placeholder='Enter email'
+          value={input.email}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3' controlId='password'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type='password'
+          placeholder='Password'
+          value={input.password}
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+
+      <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+        <Form.Check type='checkbox' label='Check me out' />
+      </Form.Group>
+
+      <Button variant='primary' type='submit'>
+        Log In
+      </Button>
+    </Form>
   )
 }
 
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import useForm from '../../hooks/useForm'
 import { registerUserService } from '../../services/userServices'
+import { Button, Form } from 'react-bootstrap'
+import useForm from '../../hooks/useForm'
 
 const Signup = () => {
   const navigate = useNavigate()
@@ -9,10 +11,11 @@ const Signup = () => {
     try {
       const response = await registerUserService(data)
       if (response.status === 200) {
+        console.log(response)
         navigate('/login')
       }
     } catch (error) {
-      console.log(error)
+      console.log('Error: ', error)
     }
   }
 
@@ -25,9 +28,78 @@ const Signup = () => {
   })
 
   return (
-    <div>Signup</div>
+    <div>
+      <Form onSubmit={handleSubmit}>
+        {/* controlId es el id del label y el input */}
+        <Form.Group className='mb-3' controlId='first_name'>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter email'
+            // id='first_name'
+            name='first_name'
+            value={input.first_name}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='last_name'>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter email'
+            // id='last_name'
+            name='last_name'
+            value={input.last_name}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Select
+          aria-label='Default select example'
+          id='gender'
+          name='gender'
+          value={input.gender}
+          onChange={handleInputChange}
+        >
+          <option>Gender</option>
+          <option value='M'>M</option>
+          <option value='F'>F</option>
+        </Form.Select>
+
+        <Form.Group className='mb-3' controlId='email'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            // id='email'
+            name='email'
+            value={input.email}
+            onChange={handleInputChange}
+          />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            // id='password'
+            name='password'
+            value={input.password}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>
+          Sign Up
+        </Button>
+      </Form>
+    </div>
   )
 }
 
 export default Signup
-// holi 🐢
